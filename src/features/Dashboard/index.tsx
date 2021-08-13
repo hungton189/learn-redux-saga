0e53380eed8580a763d3086b6756ccd4310e15ab
hungton189 <hungton987@gmail.com>
2021-08-13 21:43:58 +0700

    statistics component

--- src/features/Dashboard/index.tsx
+++ src/features/Dashboard/index.tsx
@@ -1,5 +1,8 @@
+import { Box, Grid, LinearProgress, makeStyles } from '@material-ui/core';
+import { PeopleAlt } from '@material-ui/icons';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
 import React, { useEffect } from 'react';
+import StatisticItem from './components/StatisticItem';
 import {
   dashboardActions,
   selectDashboardLoading,
@@ -9,6 +12,18 @@ import {
   selectRankingByCityList,
 } from './dashboardSlice';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'relative',
+    paddingTop: theme.spacing(1),
+  },
+  loading: {
+    position: 'absolute',
+    top: theme.spacing(-1),
+    width: '100%',
+  },
+}));
+
 export default function Dashboard() {
   const dispatch = useAppDispatch();
   const loading = useAppSelector(selectDashboardLoading);
@@ -16,15 +31,45 @@ export default function Dashboard() {
   const highestStudentList = useAppSelector(selectHighestStudentList);
   const lowestStudentList = useAppSelector(selectLowestStudentList);
   const rankingByCityList = useAppSelector(selectRankingByCityList);
-  console.log(
-    loading,
-    statistics,
-    highestStudentList,
-    lowestStudentList,
-    rankingByCityList
-  );
+
+  const classes = useStyles();
   useEffect(() => {
     dispatch(dashboardActions.fetchData());
   }, [dispatch]);
-  return <div>Dashboard</div>;
+  return (
+    <Box className={classes.root}>
+      {!loading && <LinearProgress className={classes.loading} />}
+      {/* statistics */}
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6} lg={3} xl={3}>
+          <StatisticItem
+            icon={<PeopleAlt fontSize="large" color="primary" />}
+            label="male"
+            value={statistics.maleCount}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} lg={3} xl={3}>
+          <StatisticItem
+            icon={<PeopleAlt fontSize="large" color="primary" />}
+            label="female"
+            value={statistics.femaleCount}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} lg={3} xl={3}>
+          <StatisticItem
+            icon={<PeopleAlt fontSize="large" color="primary" />}
+            label="High Mark Count"
+            value={statistics.highMarkCount}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} lg={3} xl={3}>
+          <StatisticItem
+            icon={<PeopleAlt fontSize="large" color="primary" />}
+            label="Low Mark Count"
+            value={statistics.lowMarkCount}
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
