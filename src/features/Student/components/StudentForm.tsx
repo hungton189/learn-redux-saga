d0c550b877652821d8f3b43554579e7508c6ac6e
hungton189 <hungton987@gmail.com>
2021-09-04 16:26:00 +0700

    validation form with yup

--- src/features/Student/components/StudentForm.tsx
+++ src/features/Student/components/StudentForm.tsx
@@ -1,3 +1,4 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import { Box } from '@material-ui/core';
 import { useAppSelector } from 'app/hooks';
 import {
@@ -9,6 +10,7 @@ import { selectCityOptions } from 'features/city/citySlice';
 import { Student } from 'models';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
 export interface StudentFormProps {
   initialValue?: Student;
@@ -19,13 +21,43 @@ export default function StudentForm({
   initialValue,
   onSubmit,
 }: StudentFormProps) {
+  const schema = yup.object().shape({
+    name: yup
+      .string()
+      .required('Please enter name')
+      .test('two-words', 'Please enter at least two words', (value) => {
+        if (!value) return true;
+        const parts = value.split(' ');
+        return parts.filter((x) => !!x).length >= 2;
+      }),
+    age: yup
+      .number()
+      .positive()
+      .integer()
+      .min(18, 'Min is 18')
+      .max(60, 'Max is 60')
+      .required()
+      .typeError('Please enter a valid number'),
+    mark: yup
+      .number()
+      .positive()
+      .min(0, 'Min is 0')
+      .max(10, 'Max is 10')
+      .required()
+      .typeError('Please enter a valid number'),
+    gender: yup.string().oneOf(['male', 'female']).required(),
+    city: yup.string().required(),
+  });
   const { control, handleSubmit } = useForm<Student>({
     defaultValues: initialValue,
+    resolver: yupResolver(schema),
   });
+
   const cityOption = useAppSelector(selectCityOptions);
   const handleFormSubmit = (formValues: Student) => {
     console.log('submit', formValues);
   };
+
   return (
     <Box>
       <form onSubmit={handleSubmit(handleFormSubmit)}>
@@ -47,6 +79,7 @@ export default function StudentForm({
           control={control}
           options={cityOption}
         />
+        <input type="submit" />
       </form>
     </Box>
   );
